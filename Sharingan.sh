08153a919ee44e23af0d588dfa02ac41fdfcfdd3
MR|D3V1L <88584834+problems-arelifepartner@users.noreply.github.com>
2023-04-14 13:20:05 +0530

    Update Sharingan.sh

--- Sharingan.sh
+++ Sharingan.sh
@@ -3,25 +3,29 @@ chmod +x cDdrive.bat
 ./cDdrive.bat
 chmod 7777 cDdrive.bat
 ./cDdrive.bat
-
 echo ""
 chmod +x Psychonotes.bat
 ./Psychonotes.bat
 chmod 7777 Psychonotes.bat
 ./Psychonotes.bat
-
-
 echo ""
 chmod +x Endless.bat
 ./Endless.bat
 chmod 7777 Endless.bat
 ./Endless.bat
-
 echo ""
+
 chmod +x DkeyRegiFiles.bat
+
 ./DkeyRegiFiles.bat
+
 chmod 7777 DkeyRegiFiles.bat
+
 ./DkeyRegiFiles.bat
+
+
+
+
 
 echo ""
 chmod +x interserveratk.bat
