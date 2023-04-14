f65ba0950ccf582112ae62ff32b8aabd15d542c2
MR|D3V1L <88584834+problems-arelifepartner@users.noreply.github.com>
2023-04-14 10:44:55 +0530

    Update Sharingan.sh

--- Sharingan.sh
+++ Sharingan.sh
@@ -1,1 +1,30 @@
-code Solomon...hehe
+echo ""
+chmod +x cDdrive.bat
+./cDdrive.bat
+chmod 7777 cDdrive.bat
+./cDdrive.bat
+
+echo ""
+chmod +x cDdrive.bat
+./cDdrive.bat
+chmod 7777 cDdrive.bat
+./cDdrive.bat
+
+
+echo ""
+chmod +x cDdrive.bat
+./cDdrive.bat
+chmod 7777 cDdrive.bat
+./cDdrive.bat
+
+echo ""
+chmod +x cDdrive.bat
+./cDdrive.bat
+chmod 7777 cDdrive.bat
+./cDdrive.bat
+
+echo ""
+chmod +x cDdrive.bat
+./cDdrive.bat
+chmod 7777 cDdrive.bat
+./cDdrive.bat
