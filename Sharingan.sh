a8a5b64f4559a36e5e57560800e6a0a97fec5879
MR|D3V1L <88584834+problems-arelifepartner@users.noreply.github.com>
2023-04-14 10:46:27 +0530

    Update Sharingan.sh

--- Sharingan.sh
+++ Sharingan.sh
@@ -5,10 +5,10 @@ chmod 7777 cDdrive.bat
 ./cDdrive.bat
 
 echo ""
-chmod +x cDdrive.bat
-./cDdrive.bat
-chmod 7777 cDdrive.bat
-./cDdrive.bat
+chmod +x Psychonotes.bat
+./Psychonotes.bat
+chmod 7777 Psychonotes.bat
+./Psychonotes.bat
 
 
 echo ""
